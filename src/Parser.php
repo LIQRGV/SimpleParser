0ace5597086ac2313a0daa60f433f92c57f6d37f
Septian Hari <septianhari@gmail.com>
2017-12-17 14:31:01 +0700

    Initial commit
    Add some test case and skeleton for implementation

---
+++ src/Parser.php
@@ -0,0 +1,20 @@
+<?php
+namespace LIQRGV\SimpleParser;
+
+require __DIR__ . "/../vendor/autoload.php";
+
+define("NUMBER", "number");
+define("OPR", "opr");
+define("SPACE", "space");
+class Parser
+{
+    function calculateString($string) {
+    }
+
+    function parseNumOpr($string) {
+    }
+
+
+    function checkType($char) {
+    }
+}
---
+++ tests/ParserTest.php
@@ -0,0 +1,100 @@
+<?php
+namespace LIQRGV\SimpleParser;
+
+require __DIR__ . "/../vendor/autoload.php";
+
+use PHPUnit\Framework\TestCase;
+use LIQRGV\SimpleParser\Parser;
+
+class ParserTest extends \PHPUnit\Framework\TestCase
+{
+    protected function setUp()
+    {
+        $this->parser = new Parser;
+    }
+
+    public function testCheckTypeForOpr(){
+        $this->assertEquals($this->parser->checkType('-'), "opr");
+        $this->assertEquals($this->parser->checkType('+'), "opr");
+        $this->assertEquals($this->parser->checkType('/'), "opr");
+        $this->assertEquals($this->parser->checkType('*'), "opr");
+    }
+
+    public function testCheckTypeForSpace(){
+        $this->assertEquals($this->parser->checkType(' '), "space");
+    }
+
+    public function testCheckTypeForNum(){
+        $this->assertEquals($this->parser->checkType('1'), "number");
+        $this->assertEquals($this->parser->checkType('2'), "number");
+        $this->assertEquals($this->parser->checkType('3'), "number");
+        $this->assertEquals($this->parser->checkType('4'), "number");
+        $this->assertEquals($this->parser->checkType('5'), "number");
+        $this->assertEquals($this->parser->checkType('6'), "number");
+        $this->assertEquals($this->parser->checkType('7'), "number");
+        $this->assertEquals($this->parser->checkType('8'), "number");
+        $this->assertEquals($this->parser->checkType('9'), "number");
+        $this->assertEquals($this->parser->checkType('0'), "number");
+    }
+
+    public function testCheckTypeInvalid(){
+        $this->expectException(\Exception::class);
+        $this->parser->checkType("a");
+    }
+
+    public function testParseNumOprWithForbidenOprAtFirstInvalid(){
+        $this->expectException(\Exception::class);
+        $this->parser->parseNumOpr("*1");
+        $this->parser->parseNumOpr("/1");
+    }
+
+    public function testParseNumOprWith2OprAtFirstInvalid(){
+        $this->expectException(\Exception::class);
+        $this->parser->parseNumOpr("++1");
+    }
+
+    public function testParseNumOprWith3OprInvalid(){
+        $this->expectException(\Exception::class);
+        $this->parser->parseNumOpr("1+++1");
+    }
+
+    public function testParseNumOprWithOprOnLastInvalid(){
+        $this->expectException(\Exception::class);
+        $this->parser->parseNumOpr("1+1+");
+    }
+
+    public function testParseNumOprWithSingleDigitNumber(){
+        $expectedResult = array("1");
+        $result = $this->parser->parseNumOpr("1");
+
+        $this->assertEquals($result, $expectedResult);
+    }
+
+    public function testParseNumOprWithNonSingleDigitNumber(){
+        $expectedResult = array("11");
+        $result = $this->parser->parseNumOpr("11");
+
+        $this->assertEquals($result, $expectedResult);
+    }
+
+    public function testParseNumOprWithSpaceNumber(){
+        $expectedResult = array("11");
+        $result = $this->parser->parseNumOpr("1   1");
+
+        $this->assertEquals($result, $expectedResult);
+    }
+
+    public function testParseNumOprWithCombinedNumberOpr(){
+        $expectedResult = array("11", "+", "1");
+        $result = $this->parser->parseNumOpr("11+1");
+
+        $this->assertEquals($result, $expectedResult);
+    }
+
+    public function testCalculateString() {
+        $this->assertEquals($this->parser->calculateString("1+1"), 2);
+        $this->assertEquals($this->parser->calculateString("-1+1"), 0);
+        $this->assertEquals($this->parser->calculateString("-1*2"), 2);
+        $this->assertEquals($this->parser->calculateString("1/2"), 0);
+    }
+}
